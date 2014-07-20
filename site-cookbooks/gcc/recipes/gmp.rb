1075d68103b0bf8846045b232adc82b62644ab6a
古川剛啓 <yoshi@chefManager.(none)>
2014-07-20 23:41:12 +0900

    Fixed attribute

--- site-cookbooks/gcc/recipes/gmp.rb
+++ site-cookbooks/gcc/recipes/gmp.rb
@@ -9,8 +9,8 @@
 execute "gmp_#{node['gmp']['version']}" do
 	command <<-EOH
 		cd /tmp
-		wget #{node['gmp']['gmp_url']}/gmp-#{node['gmp']['version']}#node['gmp']['version_prefix'].tar.bz2
-		tar xjf gmp-#{node['gmp']['version']}#node['gmp']['version_prefix'].tar.bz2
+		wget #{node['gmp']['gmp_url']}/gmp-#{node['gmp']['version']}#{node['gmp']['version_prefix']}.tar.bz2
+		tar xjf gmp-#{node['gmp']['version']}#{node['gmp']['version_prefix']}.tar.bz2
 		cd gmp-#{node['gmp']['version']}
 		./configure --prefix=#{node['gnu']['prefix']} --libdir=#{node['gnu']['prefix']}/#{node['gnu']['libdir']}
 		make -j4
