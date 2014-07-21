dbc78c55fbc3e1534c67517e227355eb6b5d018f
古川剛啓 <yoshi@chefManager.(none)>
2014-07-21 11:11:16 +0900

    Add recipe of Ruby

---
+++ site-cookbooks/ruby/attributes/default.rb
@@ -0,0 +1,6 @@
+default['ruby']['user'] = "ruby"
+default['ruby']['group'] = "ruby"
+default['ruby']['version'] = "1.9.3-p547"
+default['ruby']['ruby_url'] = "http://cache.ruby-lang.org/pub/ruby"
+default['ruby']['prefix'] = "/usr/local"
+
---
+++ site-cookbooks/ruby/recipes/default.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: ruby
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "Download ruby" do
+	command <<-EOH
+		log "#{Chef::Config[:file_cache_path]}"
+		cd #{Chef::Config[:file_cache_path]}
+		wget #{node['ruby']['ruby_url']}/ruby-#{node['ruby']['version']}.tar.bz2
+	EOH
+	not_if { File.exists?( "#{Chef::Config[:file_cash_path]}" ) }
+end
+
+execute "Build ruby" do
+	command <<-EOH
+		cd #{Chef::Config[:file_cache_path]}
+		tar xjf ruby-#{node['ruby']['version']}.tar.bz2
+		cd ruby-#{node['ruby']['version']}
+		./configure --prefix=#{node['ruby']['prefix']}
+		make
+		make install
+	EOH
+	not_if { File.exists?("#{node['ruby']['install_dir']}/ruby") }
+end
+
