1daf1f51437c4030ea3dc6f0eef479899bb30ed5
古川剛啓 <yoshi@chefManager.(none)>
2014-08-03 16:17:12 +0900

    Wrote a recipe.

--- site-cookbooks/graphviz/recipes/default.rb
+++ site-cookbooks/graphviz/recipes/default.rb
@@ -6,3 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+execute "prepare repo for graphviz" do
+	command "wget -O #{node['graphviz']['repo']} #{node['graphviz']['download_repo']}"
+	action :run
+	not_if { File.exists?("#{node['graphviz']['repo']}" ) }
+end
+
+%w{graphviz graphviz-lang-ocaml graphviz-lang-perl graphviz-lang-php graphviz-lang-python graphviz-lang-ruby graphviz-lang-tcl graphviz-libs graphviz-plugins-core graphviz-plugins-gd graphviz-plugins-x graphviz-qt graphviz-x ann-libs compat-readline5 freeglut gd gdbm-devel gtkglext-libs gts guile libXaw libXpm libmng netpbm ocaml ocaml-runtime phonon-backend-gstreamer php-cli php-common qt qt-sqlite qt-x11 tcl tk}.each do |pkg|
+	package pkg do
+		action :install
+	end
+end
+
