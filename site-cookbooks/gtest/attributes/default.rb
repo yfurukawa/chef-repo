3715bbef41a3b5310db7b46beb6e243d92c227cc
古川剛啓 <yoshi@chefManager.(none)>
2014-08-06 21:53:04 +0900

    Delete switch statement due to attribute cannot use that.

--- site-cookbooks/gtest/attributes/default.rb
+++ site-cookbooks/gtest/attributes/default.rb
@@ -1,11 +1,4 @@
-switch "#{node[:platform][:machine]}"
-  case "x86_64"
-    default['gtest']['lib_dir'] = "/usr/local/lib64"
-  case "i386"
-    default['gtest']['lib_dir'] = "/usr/local/lib"
-  end
-end
-
+default['gtest']['lib_dir'] = "/usr/local/lib64"
 default['gtest']['include_dir'] = "/usr/local/include"
 default['gtest']['download_url'] = "https://googlemock.googlecode.com/files"
 default['gtest']['version'] = "1.7.0"
