eea8c15f1d790c7a1550e37980a46e7736aa4eb5
古川剛啓 <yoshi@chefManager.(none)>
2014-10-13 22:33:19 +0900

    Update patches and recipe.

--- site-cookbooks/cpplint/recipes/default.rb
+++ site-cookbooks/cpplint/recipes/default.rb
@@ -11,9 +11,23 @@ remote_file "#{Chef::Config[:file_cache_path]}/cpplint.py" do
   source "http://google-styleguide.googlecode.com/svn/trunk/cpplint/cpplint.py"
 end
 
+cookbook_file "#{Chef::Config[:file_cache_path]}/cpplintDir.py.patch" do
+  source "cpplintDir.py.patch"
+end
+  
+cookbook_file "#{Chef::Config[:file_cache_path]}/cpplint.py.patch" do
+  source "cpplint.py.patch"
+end
+  
 execute "patching cpplint for auto execution" do
   command <<-EOH
-    cd #{Chef::Config[:file_chache_path]}
+    cd "#{Chef::Config[:file_cache_path]}"
+    patch -p0 -o cpplintDir.py < cpplintDir.py.patch
+    patch -p0 < cpplint.py.patch
+    mv cpplint.py /usr/local/bin
+    mv cpplintDir.py /usr/local/bin
+    chmod 0555 /usr/local/bin/cpplint*
   EOH
+  not_if { File.exists?("/usr/local/bin/cpplintDir.py") }
 end
 
