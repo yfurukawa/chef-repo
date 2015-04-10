7558fd7c0c31f651fe9d92df86d901d4a3d84eec
Yoshihiro Furukawa <yo4.furukawa+github@gmail.com>
2015-04-11 00:53:11 +0900

    change package name due to made mistake

--- site-cookbooks/openjdk/recipes/default.rb
+++ site-cookbooks/openjdk/recipes/default.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-%w{java-1.8.0-openjdk java-1.8.0-openjdk-javadoc java-1.8.0-openjdk-manual}.each |pkg| do
+%w{java-1.8.0-openjdk java-1.8.0-openjdk-javadoc java-1.8.0-openjdk-devel}.each do |pkg|
   package pkg do
     action :install
   end
