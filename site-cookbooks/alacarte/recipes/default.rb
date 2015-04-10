4dd43db130849566dd28ba9ed86d86aa04e329e5
Yoshihiro Furukawa <yo4.furukawa+github@gmail.com>
2015-04-10 22:28:24 +0900

    Add recipe for alacarte whtch is gnome menu editor.

---
+++ site-cookbooks/alacarte/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: alacarte
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "alacarte" do
+	action :install
+end
+
