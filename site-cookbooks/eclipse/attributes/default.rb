8d0e12ef08dbf01f2a7cc6d1aad85ecace165ba9
Yoshihiro Furukawa <yo4.furukawa+github@gmail.com>
2015-04-10 22:48:32 +0900

    Update target eclipse version from kepler to kepler with java8 patch.

--- site-cookbooks/eclipse/attributes/default.rb
+++ site-cookbooks/eclipse/attributes/default.rb
@@ -1,6 +1,6 @@
-default['eclipse']['eclipse_base'] = "eclipse-standard-kepler-SR2-linux-gtk-x86_64"
+default['eclipse']['eclipse_base'] = "eclipse-java-kepler-SR2-Java8-linux-gtk-x86_64"
 default['eclipse']['eclipse_home'] = "/usr/local/bin"
-default['eclipse']['eclipse_download_url'] = "http://ftp.jaist.ac.jp/pub/eclipse/technology/epp/downloads/release/kepler/SR2"
+default['eclipse']['eclipse_download_url'] = "http://ftp.jaist.ac.jp/pub/eclipse/technology/epp/downloads/release/kepler/SR2-with-Java8-patches"
 default['eclipse']['cdt_version'] = "8.3.0"
 default['eclipse']['cdt_download_url'] = "http://ftp.jaist.ac.jp/pub/eclipse/tools/cdt/releases/kepler/sr2"
 default['eclipse']['pleiadesFile'] = "pleiades.zip"
